5e0094355311fbaebc70bfa2afdf66696ff13838
Raymond Mendoza <raymondoza28@gmail.com>
2024-10-04 01:10:03 -0500

    Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import LikedSongs from './LikedSongs.js';
+import Spotify from './Spotify.js';
+import './App.css'
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <><><Navbar
+          expand="lg"
+          className="navbar border-body navbar-expand-md"
+          bg="dark"
+          data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand href="#home"><h3>MySongs</h3></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <LikedSongs /></>
+    <Spotify /></>
+  )  
 }
 
 export default App;
